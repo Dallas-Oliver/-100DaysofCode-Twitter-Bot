2c4cbd11629d1a679ff4f9425e1459818abebf3e
Dallas <dallas.oliver91@gmail.com>
2019-07-03 13:34:18 -0400

    added comments for practice documneting projects

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const Twit = require("twit");
 const config = require("./config");
-const database = require("nedb");
 const T = Twit(config);
 
 const track = {
-  track: "#100DaysofCode"
+  track: ["#100DaysofCode", "#javscript", "#frontend"]
 };
 
 const stream = T.stream("statuses/filter", track);
 stream.on("tweet", tweeted);
 
 function tweeted(eventMessage) {
-  console.log(eventMessage.id, eventMessage);
+  console.log(eventMessage.user.screen_name, eventMessage.text);
+
   tweet(eventMessage.id_str);
 }
 
 function tweet(tweet_id) {
   function callback(err, data, res) {
-    console.log("the bot is running", data);
+    console.log("the bot is running");
   }
 
   T.post("statuses/retweet/:id", { id: tweet_id }, callback);
